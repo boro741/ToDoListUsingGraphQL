134b544ba6d3a8f68489a903d369cca87638de58
Pavan Boro <Pavan.Boro@resideo.com>
2020-12-29 15:05:56 +0530

    Mutation Create and some refactor and fixes for previous functionality

--- src/index.js
+++ src/index.js
@@ -1,109 +1,146 @@
 import {GraphQLServer} from 'graphql-yoga';
-
-// Having difficulty in implementing schema and resolvers
-// 1. Is for every schema I need to implement resolver? YES
-// 2. What to define inside Query in schema?            things you want to query
-// 3. What to implement inside Query in resolver?       things you've declared inside query schema   
-// 4. What to define outside Query in schema?           custom object types 
-// 5. What to implement outside Query in resolver?      Associating a relationships between objects
-// 6. How to associate One object with another?         using a property of parent object and define outside Query in resolvers  
-
+import uuidv4 from "uuid/v4";
 // DB
 const Todos = [{
-    id: 1,
+    id: '1',
     title: "Learn DS",
 },
 {
-    id: 2,
+    id: '2',
     title: "Prepare resume",
 },
 {
-    id: 3,
+    id: '3',
     title: "Learn Science",
 },
 ];
 
 const Tasks = [
 {
-    id: 1,
+    id: '1',
     task:[
         "1. Arrays",
         "2. Trees",
         "3. Dynamic Programming"
     ],
-    title: 1
+    title: '1'
 },
 {
-    id: 2,
+    id: '2',
     task:[
         "1. Prepare resume for Amazon",
         "2. Prepare resume for Flipkart"
     ],
-    title: 2
+    title: '2'
 },
 {
-    id: 3,
+    id: '3',
     task:[
         "1. Learn Thermodynamics",
         "2. Learn Electrostatics"
     ],
-    title: 3
+    title: '3'
 }
 ];
 
-
+// Having difficulty in writing mutation in schema when there are association between object properties
 const typeDefs = `
     type Query{
-        todo: [Todo!]!
+        todo(title: String): [Todo!]!
         task(title: String): [Task!]!
+    }
+
+    type Mutation {
+        createTodoList(title: String!): Todo!
+        addTaskTodoList(title: String! ,tasks: [String!]!): Task!
     }
 
     type Todo {
         id: ID!
         title: String!
-        tasks: [Task!]!
+        tasks: [Task]
     }
 
     type Task {
         id: ID!
         task: [String!]!
-        title: Todo!
+        title: Todo
     }
 `;
 
 
 const resolvers  = {
     Query: {
-        todo(){
-            return Todos;
+        todo(parent, args, ctx, info){
+            if(args.title == null) 
+                return Todos;
+
+            return Todos.filter((todo)=>{
+                return todo.title.toLowerCase().includes(args.title.toLowerCase());
+            });
         },
         task(parent, args, ctx, info){
             if(args.title == null){
                 return Tasks;
             }
 
-            // Had difficulty in retrieving property of another object using foreign key
             return Tasks.filter((task) => {
                 return Todos.find((todo) => todo.id === task.title).title.toLowerCase().includes(args.title.toLowerCase());
             });
         }
     },
+    Mutation: {
+        createTodoList(parent, args, ctx, info){
+            const titleTaken = Todos.some((todo)=>{
+                return todo.title === args.title;
+            });
 
-    // Associating between Task title with Parent Object (Todo) title   
+            if(titleTaken){
+                throw new Error(`Title with "${args.title}" already exists.`);
+            }
+
+            const todo = {
+                id: uuidv4(),
+                title: args.title
+            }
+
+            Todos.push(todo);
+
+            return todo;
+        },
+        addTaskTodoList(parent, args, ctx, info){
+            const todoListExist = Todos.some((todo)=>{
+                return todo.id === args.title;
+            });
+
+            if(!todoListExist)
+                throw new Error(`Todo List with "${args.title}" does not exists.`);
+            
+            const task = {
+                id: uuidv4(),
+                task: args.tasks,
+                title: args.title
+            };
+            
+            Tasks.push(task);
+            return task;
+        }
+    },
+
     Task: {
         title(parent, args, ctx, info) {
+            // Here parent is Task Object
             return Todos.find((todo) => {
-                return todo.id == parent.id;
+                return todo.id === parent.title;
             });
         }
         
     },
 
-    // Associating between Task task property with Parent Object (Task) task prpoerty
     Todo: {
         tasks(parent, args, ctx, info){
             return Tasks.filter((task)=>{
-                return task.title == parent.id;
+                return task.title === parent.id;
             });
         }
     }
